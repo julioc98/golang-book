e46c7d8262e200afe9c27bf9f3b96c216da1b309
julioc98 <julioc.santos98@gmail.com>
2017-08-29 21:36:32 -0300

    chapter4 exemple 5 - instruction if

--- chapter4/main.go
+++ chapter4/main.go
@@ -6,6 +6,12 @@ import (
 
 func main() {
 	for i := 1; i <= 10; i++ {
-		fmt.Println(i)
+		if i%2 == 0 {
+			//par
+			fmt.Println(i, "even")
+		} else {
+			fmt.Println(i, "odd")
+			//ímpar
+		}
 	}
 }
